3528bd02e2b24fa7e9733db8f7627edd020a5392
quinxii <82009267+quinxii@users.noreply.github.com>
2024-03-02 17:15:32 +0700

    login_widget

--- lib/widgets/login_widget.dart
+++ lib/widgets/login_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:student_hub/constants/colors.dart';
 import 'package:student_hub/model/user_model.dart';
 import 'package:student_hub/widgets/header_nav_widget.dart';
 
@@ -6,39 +7,70 @@ class LoginWidget extends StatelessWidget {
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
-  @override
+@override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: HeaderNavBar(),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: usernameController,
-              decoration: InputDecoration(
-                labelText: 'Username',
+      body: Theme(
+        data: ThemeData.light().copyWith(
+          colorScheme: const ColorScheme.light(
+            primary: tdNeonBlue,
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 14.0),
+                child: Center(
+                  child: Text(
+                    'Login with StudentHub',
+                    style: TextStyle(
+                      fontSize: 20, 
+                      fontWeight: FontWeight.w600,
+                      fontFamily: 'Roboto'
+                    ),
+                  ),
+                ),
               ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
+              TextField(
+                controller: usernameController,
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                ),
               ),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton( 
-              onPressed: () {
-                String username = usernameController.text;
-                String password = passwordController.text;
-                User user = User(username: username, password: password);
-                // Here you can perform login logic
-              },
-              child: Text('Login'),
-            ),
-          ],
+              SizedBox(height: 16.0),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+              ),
+              SizedBox(height: 30.0),
+              ElevatedButton(
+                onPressed: () {
+                  String username = usernameController.text;
+                  String password = passwordController.text;
+                  User user = User(username: username, password: password);
+                  // Here you can perform login logic
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: tdNeonBlue, 
+                  minimumSize: Size(double.infinity, 50),
+                ),
+                child: Text(
+                  'Sign In',
+                  style: TextStyle(
+                    fontSize: 16, // Set font size
+                    color: Colors.white, // Set text color
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
