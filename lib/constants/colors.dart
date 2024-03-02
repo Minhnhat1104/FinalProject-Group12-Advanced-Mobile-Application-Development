c458559b23f7966cf7e5670bafa45a71e927866d
quinxii <82009267+quinxii@users.noreply.github.com>
2024-03-02 15:49:27 +0700

    set up Color

---
+++ lib/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const Color tdNeonBlue = Color(0xFF5465FF);
+const Color tdCornBlue = Color(0xFF788BFF);
+const Color tdJordyBlue = Color(0xFF9BB1FF);
+const Color tdPeriBlue = Color(0xFFBFD7FF);
+const Color tdCyanBlue = Color(0xFFE2FDFF);
+
+const Color tdText = Color(0xFF3A3A3A);
+
+const Color tdBlack = Color(0x0000000);
+const Color tdGrey = Color(0xFF717171);
+const Color tdWhite = Color(0xFFFFFFFF);
