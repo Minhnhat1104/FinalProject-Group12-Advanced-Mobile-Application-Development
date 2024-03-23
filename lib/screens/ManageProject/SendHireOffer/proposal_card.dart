3bcaa21fab5796ae25d12fc771e73acfeea9f1d5
quinxii <82009267+quinxii@users.noreply.github.com>
2024-03-23 14:11:41 +0700

    separate proposal_card to another dart file

---
+++ lib/screens/ManageProject/SendHireOffer/proposal_card.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:student_hub/constants/colors.dart';
+import 'package:student_hub/model/proposal_model.dart';
+
+class ProposalCard extends StatelessWidget {
+  final Proposal proposal;
+
+  const ProposalCard({required this.proposal});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        elevation: 0,
+        shadowColor: Colors.transparent,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                children: [
+                  Icon(
+                    Icons.person,
+                    color: tdNeonBlue,
+                    size: 40, 
+                  ),
+                  SizedBox(width: 16),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(proposal.name),
+                      SizedBox(height: 6),
+                      Text(proposal.title),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  Text(proposal.job),
+                  Spacer(),
+                  Text(proposal.text),
+                ],
+              ),
+              SizedBox(height: 16),
+              Text(
+                proposal.description,
+              ),
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(vertical: 8), 
+                          child: Text('Message'),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10), 
+                          ),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(vertical: 8),
+                          child: Text('Hire'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Divider( 
+                color: const Color.fromARGB(255, 206, 206, 206), 
+                thickness: 0.8, 
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/ManageProject/SendHireOffer/send_hire_offer_screen.dart
+++ lib/screens/ManageProject/SendHireOffer/send_hire_offer_screen.dart
@@ -3,6 +3,7 @@ import 'package:student_hub/constants/colors.dart';
 import 'package:student_hub/model/project_posting_model.dart';
 import 'package:student_hub/model/proposal_model.dart';
 import 'package:student_hub/screens/ManageProject/ProjectDetail/project_detail_screen.dart';
+import 'package:student_hub/screens/ManageProject/SendHireOffer/proposal_card.dart';
 import 'package:student_hub/widgets/header_nav_widget.dart';
 
 class ProposalsView extends StatefulWidget {
@@ -186,102 +187,3 @@ class _ProposalsViewState extends State<ProposalsView> {
   }
 }
 
-class ProposalCard extends StatelessWidget {
-  final Proposal proposal;
-
-  const ProposalCard({required this.proposal});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Card(
-        elevation: 0,
-        shadowColor: Colors.transparent,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Row(
-                children: [
-                  Icon(
-                    Icons.person,
-                    color: tdNeonBlue,
-                    size: 40, 
-                  ),
-                  SizedBox(width: 16),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(proposal.name),
-                      SizedBox(height: 6),
-                      Text(proposal.title),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(height: 16),
-              Row(
-                children: [
-                  Text(proposal.job),
-                  Spacer(),
-                  Text(proposal.text),
-                ],
-              ),
-              SizedBox(height: 16),
-              Text(
-                proposal.description,
-              ),
-              SizedBox(height: 16),
-              Row(
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 8),
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(vertical: 8), 
-                          child: Text('Message'),
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: 8),
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 8),
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10), 
-                          ),
-                        ),
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(vertical: 8),
-                          child: Text('Hire'),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-              Divider( 
-                color: const Color.fromARGB(255, 206, 206, 206), 
-                thickness: 0.8, 
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
